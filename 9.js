254a8a3520ac1577c12b8f2abc856a340415bc85
Donnie Armstrong <donaldjarmstrong@gmail.com>
2014-09-25 15:45:31 -0500

    adding solution 9

--- 9.js
+++ 9.js
@@ -3,8 +3,6 @@ var http = require('http');
 http.get(process.argv[2], function (response) {
 	var bufferedData = "";
 
-	response.setEncoding('utf8');
-
 	response.on('error', function (e) {
 		console.error(e);
 	});
